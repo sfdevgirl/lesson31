fba046b76ca20a7498831bcb1b1b7f089b005144
Shannon Smith <shannones2013@gmail.com>
2015-04-22 16:19:52 -0700

    code

--- script.js
+++ script.js
@@ -2,20 +2,14 @@ $( document ).ready(function() {
 	for (var i=1; i <= 100; i++) {
 
 		if (i % 15 == 0) {
-		$(body).append("fizzbuzz");
-		
-
-	}
-		else if (i % 3 == 0) {
-		$(body).append("fizz");
-		
-	}
-		else if (i % 5 == 0) {
-		$(body).append("buzz");
-		
-	}
-		else 
-		$(body).append(i);
+			$(body).append("fizzbuzz");
+		} else if (i % 3 == 0) {
+			$(body).append("fizz");
+		} else if (i % 5 == 0) {
+			$(body).append("buzz");
+		} else {
+			$(body).append(i);
+		}
 		$(body).append("<br>");
-  }
+  	}
 });
